06fde130f8be137af6ce74d8b7f34ec0e2006fbe
Junked <junked.auto@gmail.com>
2023-06-25 00:01:16 +0900

    add azo birthday

---
+++ docs/an/birthday/js/index.js
@@ -0,0 +1,89 @@
+var isBirthday = false;
+var debug = false;
+let dbg = getParam("debug");
+if (dbg === "1") { 
+    debug = true;
+}
+
+var headWrap = document.getElementById("count_down_birthday");
+var headWrapMsg = document.getElementById("count_down_message");
+var headWrapTmr = document.getElementById("count_down_timer");
+
+var bDayMsg = document.getElementById("birthday_message");
+var bDayNets = document.getElementById("netsurugi_dono_rainbow");
+var bDayAA = document.getElementById("netsurugi_dono_rainbow_aa");
+
+ function countToBirthday(bMonth, bDay, element_id) {
+    let now = new Date();
+    let year = now.getFullYear();
+
+    let bDayStart = Date.parse( year+"-"+bMonth.toString().padStart(2, '0')+"-"+bDay.toString().padStart(2, '0')+"T00:00:00+0900" );
+    let bDayEnd = Date.parse( year+"-"+bMonth.toString().padStart(2, '0')+"-"+bDay.toString().padStart(2, '0')+"T23:59:59+0900" );
+
+    let msg ="誕生日まで残り: ";
+    let remain = 0;
+    if (bDayStart > now.getTime()) {
+        console.log("誕生日前");
+        msg = getNextBdayCount(bDayStart, now);
+        console.log(msg);
+    } else if (now.getTime() > bDayEnd) {
+        console.log("誕生日後");
+        let nextBday = Date.parse( (year + 1)+"-"+bMonth.toString().padStart(2, '0')+"-"+bDay.toString().padStart(2, '0')+"T00:00:00+0900" );
+        msg = getNextBdayCount(nextBday, now);
+        console.log(msg);
+    } else {
+        console.log("誕生日！");
+        isBirthday = true;
+        return;
+    }
+
+    console.log(now.getTime());
+    console.log(bDayStart);
+    console.log(bDayEnd);
+    document.getElementById(element_id).innerHTML = msg;
+    isBirthday = false;
+}
+setInterval('countToBirthday(7, 7, "count_down_timer")',49);
+
+function getNextBdayCount(nextBday, now) {
+    remain = nextBday - now.getTime();
+
+    let day = Math.floor(remain / 1000 / 60 / 60/ 24);
+    let hour = Math.floor(remain / 1000 / 60 / 60) % 24;
+    let min  = Math.floor(remain / 1000 / 60) % 60;
+    let sec  = Math.floor(remain / 1000) % 60;
+    msg = day + "日" + hour + "時間" + min + "分" + sec + "秒";
+    return msg;
+}
+
+function checkBirthday() {
+    if (isBirthday === true || debug === true) {
+        headWrap.style.display = "none";
+        headWrapMsg.style.display = "none";
+        headWrapTmr.style.display = "none";
+
+        bDayMsg.style.display = "block";
+        bDayNets.style.display = "block";
+        bDayAA.style.display = "block";
+    } else {
+        headWrap.style.display = "flex";
+        headWrapMsg.style.display = "flex";
+        headWrapTmr.style.display = "flex";
+
+        bDayMsg.style.display = "none";
+        bDayNets.style.display = "none";
+        bDayAA.style.display = "none";
+    }
+}
+
+setInterval('checkBirthday()',49);
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
